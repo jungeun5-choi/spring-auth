0c6e1920a83542bca4da1752f1dec10d5eb32b69
jungeun-choi <jungeun5.choi@gmail.com>
2023-06-19 20:23:52 +0900

    JWT 1 회원가입 2 로그인 구현

---
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -0,0 +1,55 @@
+package com.sparta.springauth.controller;
+
+import com.sparta.springauth.dto.LoginRequestDto;
+import com.sparta.springauth.dto.SignupRequestDto;
+import com.sparta.springauth.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user/login-page")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/user/signup")
+    public String signupPage() {
+        return "signup";
+    }
+
+    @PostMapping("/user/signup")
+    public String signup(SignupRequestDto requestDto) {
+        userService.signup(requestDto);
+
+        return "redirect:/api/user/login-page";
+    }
+
+    @PostMapping("/user/login")
+    public String login(LoginRequestDto requestDto, HttpServletResponse res) {
+
+        // 검증
+        try{
+            userService.login(requestDto, res);
+
+        } catch (Exception e) {
+            // 실패하면 로그인 페이지로 redirect + error
+            return "redirect:/api/user/login-page?error";
+        }
+
+
+        // main으로 redirect
+        return "redirect:/";
+    }
+}
