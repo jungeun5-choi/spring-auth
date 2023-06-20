f4853de1167db1b2840561e06fb2ceb76a19ecb0
jungeun-choi <jungeun5.choi@gmail.com>
2023-06-20 13:26:25 +0900

    Validation 예외처리

--- src/main/java/com/sparta/springauth/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/springauth/dto/SignupRequestDto.java
@@ -1,13 +1,21 @@
 package com.sparta.springauth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SignupRequestDto {
+    @NotBlank
     private String username;
+    @NotBlank
     private String password;
+    // @Pattern() -> 정규표현식으로 체크
+    @Email    
+    @NotBlank
     private String email;
     private boolean admin = false;
     private String adminToken = "";
